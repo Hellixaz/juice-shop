79bdbaba5920425eb736fb81fc72ba6de18a2349
Barbaros Kılıçarslan <hellixaz@users.noreply.github.com>
2023-10-20 20:37:50 +0300

    Update SQL query
    Issue fix

--- data/static/codefixes/unionSqlInjectionChallenge_1.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_1.ts
@@ -1,18 +1,23 @@
-module.exports = function searchProducts () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    criteria.replace(/"|'|;|and|or/i, "")
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
-      .then(([products]: any) => {
-        const dataString = JSON.stringify(products)
-        for (let i = 0; i < products.length; i++) {
-          products[i].name = req.__(products[i].name)
-          products[i].description = req.__(products[i].description)
-        }
-        res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
-        next(error.parent)
+module.exports = function searchProducts() {
+  return (req, res, next) => {
+    let criteria = req.query.q || ''; 
+    criteria = criteria.slice(0, 200);
+    const query = `SELECT * FROM Products WHERE (name LIKE :criteria OR description LIKE :criteria) AND deletedAt IS NULL ORDER BY name`;
+    models.sequelize.query(query, {
+      replacements: { criteria: `%${criteria}%` },
+      type: models.sequelize.QueryTypes.SELECT,
+    })
+      .then((products) => {
+        products = products.map((product) => {
+          return {
+            name: req.__(product.name),
+            description: req.__(product.description),
+          };
+        });
+        res.json(utils.queryResultToJson(products));
       })
-  }
-}
+      .catch((error) => {
+        next(error);
+      });
+  };
+};
