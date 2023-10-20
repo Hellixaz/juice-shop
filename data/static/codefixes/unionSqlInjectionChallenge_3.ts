e718134b14993cb0ffd924dc25814a996fda86c1
Barbaros Kılıçarslan <hellixaz@users.noreply.github.com>
2023-10-20 21:21:56 +0300

    Update unionSqlInjectionChallenge_3.ts
    Issue fix #39

--- data/static/codefixes/unionSqlInjectionChallenge_3.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_3.ts
@@ -1,22 +1,33 @@
-module.exports = function searchProducts () {
+import { Request, Response, NextFunction } from 'express';
+
+module.exports = function searchProducts() {
   return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    // only allow apple or orange related searches
-    if (!criteria.startsWith("apple") || !criteria.startsWith("orange")) {
-      res.status(400).send()
-      return
+    let criteria: string = req.query.q ?? '';
+    criteria = criteria.substring(0, 200);
+
+    
+    if (!criteria.startsWith("apple") && !criteria.startsWith("orange")) {
+      res.status(400).send();
+      return;
     }
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
-      .then(([products]: any) => {
-        const dataString = JSON.stringify(products)
+
+    models.sequelize.query(
+      `SELECT * FROM Products WHERE (name LIKE :criteria OR description LIKE :criteria) AND deletedAt IS NULL ORDER BY name`,
+      {
+        replacements: { criteria: `%${criteria}%` },
+        type: models.sequelize.QueryTypes.SELECT,
+      }
+    )
+      .then((products: any) => {
+        const dataString = JSON.stringify(products);
         for (let i = 0; i < products.length; i++) {
-          products[i].name = req.__(products[i].name)
-          products[i].description = req.__(products[i].description)
+          products[i].name = req.__(products[i].name);
+          products[i].description = req.__(products[i].description);
         }
-        res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
-        next(error.parent)
+        res.json(utils.queryResultToJson(products));
       })
-  }
-}
+      .catch((error: ErrorWithParent) => {
+        next(error.parent);
+      });
+  };
+};
